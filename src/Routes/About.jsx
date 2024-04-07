6cf1e688326724d3248a1e4fc3432c642b7809df
Andy <68301335+Lancito01@users.noreply.github.com>
2024-04-07 03:26:31 -0300

    added age calculation to about me page

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -3,13 +3,23 @@ import Images from "../Components/common/ImportImages"
 import './About.css'
 
 const About = () => {
+  function getAge() {
+    const today = new Date();
+    const birthDate = new Date('2004-01-08');
+    let age = today.getFullYear() - birthDate.getFullYear();
+    if (today.getMonth() < birthDate.getMonth() || (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
   return (
     <div className="margin-left about-div">
       <h1>👋 Hi, there! My name&apos;s Andy.</h1>
       <h2><span className="accent-color">&gt;</span> I am a beginner full-stack programmer, looking to improve.</h2>
       <h3><span className="accent-color">&gt;</span> A little about me</h3>
       <ul>
-        <p><span className="secondary-color">&gt;</span> My name is Andy, and I&apos;m a {/*don't forget to add this*/}-year-old guy from Argentina.</p>
+        <p><span className="secondary-color">&gt;</span> My name is Andy, and I&apos;m a {getAge()}-year-old guy from Argentina.</p>
         <p><span className="secondary-color">&gt;</span> I&apos;m a beginner full-stack programmer trying to improve and broaden my skills.</p>
       </ul>
       <h3><span className="accent-color">&gt;</span> Programming languages I know</h3>
