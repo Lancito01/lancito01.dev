3b267d0f95a221d2691beaba26c99ed7dfb1b3f1
Andy <68301335+Lancito01@users.noreply.github.com>
2024-04-09 19:39:02 -0300

    slight fix to footer and moved all footer related stuff to Footer.css

--- src/Components/layout/Footer.jsx
+++ src/Components/layout/Footer.jsx
@@ -1,8 +1,9 @@
 import Images from '../common/ImportFooterImages'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className='margin-left' id='socials'>
+    <footer className='margin-left' id='socials'>
       <h3><span className='accent-color'>&gt;</span> Socials 📱</h3>
       <ul className='socials'>
         <a href="https://github.com/lancito01"><img src={Images.githubLogo} alt="github logo"></img>Github: lancito01</a>
@@ -10,7 +11,7 @@ const Footer = () => {
         <a href="https://discord.gg/9vzATnaM9c"><img src={Images.discordLogo} alt="discord logo"></img>Discord server</a>
         <a href="https://twitter.com/lancito01"><img src={Images.twitterLogo} alt="twitter logo"></img>Twitter: @lancito01</a>
       </ul>
-    </div>
+    </footer>
   )
 }
 
