8bca9e5e293a31b460b345c30cda9f4c3571c151
Andy <andy@lancito01.dev>
2024-07-19 21:50:44 -0300

    oopsies

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AndyScript from "./Routes/AndyScript";
 
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
+import Ball from "./Routes/Ball";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/about" element={<About />}></Route>
             <Route path="/andybot" element={<AndyBot />}></Route>
             <Route path="/andyscript" element={<AndyScript />}></Route>
+            <Route path="/ball" element={<Ball />}></Route>
 
           </Route>
         </Routes >
