9cdc88fc0f6241a5a94230bcc3e52a7c6b1cd08c
Andy <andy@lancito01.dev>
2024-07-19 21:50:23 -0300

    added /ball

---
+++ src/Routes/Ball.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+
+const Ball = () => {
+    useEffect(() => {
+        window.location.href = 'https://www.youtube.com/watch?v=v6gKBSuQjpg'
+    })
+
+    return (
+        <div>Ball</div>
+    )
+}
+
+export default Ball
