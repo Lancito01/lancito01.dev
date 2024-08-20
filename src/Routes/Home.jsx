021f1461de1ab76aa4274e2439fff2e7e19089e1
Andy <andy@lancito01.dev>
2024-08-20 02:24:48 -0300

    width fit content for links in home

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
       <h2 className='text-xl pt-2'>Hey, traveler! 🔎</h2>
       <h3 className='text-l pt-2'><span className='accent-color'>&gt;</span> Where do you wanna go? 🗺️</h3>
       <ul className='flex flex-col ml-5 pt-2'>
-        <Link to='/about'><span className="secondary-color">&gt;</span> About me</Link>
-        <Link to='/andybot'><span className="secondary-color">&gt;</span> AndyBot</Link>
-        <Link to='/andyscript'><span className="secondary-color">&gt;</span> AndyScript</Link>
-        <Link to='/games'><span className="secondary-color">&gt;</span> Games</Link>
+        <Link className="w-fit" to='/about'><span className="secondary-color">&gt;</span> About me</Link>
+        <Link className="w-fit" to='/andybot'><span className="secondary-color">&gt;</span> AndyBot</Link>
+        <Link className="w-fit" to='/andyscript'><span className="secondary-color">&gt;</span> AndyScript</Link>
+        <Link className="w-fit" to='/games'><span className="secondary-color">&gt;</span> Games</Link>
       </ul>
     </div>
   )
