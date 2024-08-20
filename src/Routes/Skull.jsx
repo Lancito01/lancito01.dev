4e75ca424ccdd64ea36e04d3eda8f512966f6a49
Andy <andy@lancito01.dev>
2024-08-20 04:51:57 -0300

    added missing file

---
+++ src/Routes/Skull.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+
+const Skull = () => {
+    useEffect(() => {
+        window.location.href = 'https://www.youtube.com/watch?v=v6gKBSuQjpg';
+    }, []);
+
+    return null;
+}
+
+export default Skull
