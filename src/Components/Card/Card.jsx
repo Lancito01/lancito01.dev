b8f8c4a825eedfee14afcf8e4518e5369ea36757
Andy <andy@lancito01.dev>
2024-11-23 19:42:11 -0300

    attempting to rotate card borders with mouse coordinates

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,7 +3,25 @@ import './Card.scss';
 
 const Card = ({ title, description, link, github = "", download = "", information = "", playLink = "" }) => {
     return (
-        <div className="card flex flex-col justify-between project w-60 sm:w-96 p-4 m-4 rounded-md">
+        <div className="card flex flex-col justify-between project w-60 sm:w-96 p-4 m-4 rounded-md" onMouseMove={(e) => {
+            const currTarget = e.currentTarget;
+
+            // const cardXMin = 0;
+            const cardXMax = currTarget.getBoundingClientRect().width;
+            const clientX = e.clientX - currTarget.getBoundingClientRect().x;
+            let result = clientX / cardXMax * 2 - 1;
+            result = result * 45;
+            currTarget.style.transform = `rotateY(${result}deg)`;
+
+            // const cardYMin = 0;
+            //const cardYMax = currTarget.getBoundingClientRect().height;
+            //const clientY = e.clientY - currTarget.getBoundingClientRect().y;
+            //result = clientY / cardYMax * 2 - 1;
+            //result = result * 45;
+            //currTarget.style.transform += ` rotateX(${-result}deg)`;
+        }} onMouseLeave={(e) => {
+            e.currentTarget.style.transform = "rotateX(0deg) rotateY(0deg)";
+        }}>
             <a
                 className="text-xl project-title mx-auto w-fit block"
                 href={"/" + link}
